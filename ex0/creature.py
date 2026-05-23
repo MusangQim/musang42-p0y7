c1b8df91a20eb7744572e5e3800bcf1e6d4ebe86
MusangQim <musangqim03@gmail.com>
2026-05-24 00:29:49 +0800

    update: attack method in each classes and fix light error and speliing problem

--- ex0/creature.py
+++ ex0/creature.py
@@ -18,17 +18,29 @@ class Flameling(Creature):
     def __init__(self, name="Flameling", type="Fire") -> None:
         super().__init__(name, type)
 
+    def attack(self):
+        return ("Flameling uses Ember!")
+
 
 class Pyrodon(Creature):
     def __init__(self, name="Pyrodon", type="Fire/Flying") -> None:
         super().__init__(name, type)
 
+    def attack(self) -> str:
+        return ("Pyrodon uses Flamethrower!")
 
-class Aquahub(Creature):
+
+class Aquabub(Creature):
     def __init__(self, name="Aquahub", type="Water") -> None:
         super().__init__(name, type)
+
+    def attack(self) -> str:
+        return ("Aquabub uses Water Gun!")
 
 
 class Torragon(Creature):
     def __init__(self, name="Torragon", type="Water") -> None:
         super().__init__(name, type)
+
+    def attack(self) -> str:
+        return ("Torragon uses Hydro Pump!")
