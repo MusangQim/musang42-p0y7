6f702d726bfd26932be07f5f546cb1636140d9c0
MusangQim <musangqim03@gmail.com>
2026-05-25 14:08:32 +0800

    capacitor added

---
+++ capacitor.py
@@ -0,0 +1,11 @@
+def main() -> None:
+    print("Testing Creature with healing capability")
+    print(" base:")
+    print(" evolved:")
+    print("Testing Creature with transform capability")
+    print(" base:")
+    print(" evolved:")
+
+
+if __name__ == "__main__":
+    main()
