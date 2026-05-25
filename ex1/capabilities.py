707f7335f3f6204b39abbfceac962e3bb21a90b9
MusangQim <musangqim03@gmail.com>
2026-05-25 14:09:15 +0800

    added file in ex1

---
+++ ex1/capabilities.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+
+
+class HealCapability():
+    def heal() -> str:
+
+
+class TransfromCapability():
+    def __init__(self, transform: bool = False) -> None:
+        self.transform = transform
+
+    def transform() -> str:
+
+        
+    def revert() -> str:
