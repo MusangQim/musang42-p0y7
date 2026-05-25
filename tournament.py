0ff74a7014b7cbd72d54678d1bae07b2aaa31ce7
MusangQim <musangqim03@gmail.com>
2026-05-25 17:47:12 +0800

    tournament added

---
+++ tournament.py
@@ -0,0 +1,7 @@
+from ex0 import FlameFactory, AquaFactory
+from ex1 import HealingCreatureFactory, TransformCreatureFactory
+from ex2 import NormalStrategy, AggresiveStrategy, DefensiveStrategy
+from ex0.factory import CreatureFactory
+from ex2.strategy import BattleStrategy
+from typing import Tuple, List
+
