2f42b9cc8faf06c8279df8b5f1df6d66e1504a94
MusangQim <musangqim03@gmail.com>
2026-05-25 16:33:50 +0800

    finalize: testHeal and testTransfrom classes created with main function

--- capacitor.py
+++ capacitor.py
@@ -1,10 +1,43 @@
+from ex1.factory import HealingCreatureFactory, TransformCreatureFactory
+
+
+def test_heal(factory: HealingCreatureFactory) -> None:
+    base = factory.create_base()
+    evolve = factory.create_evolved()
+    print(" base:")
+    print(base.describe())
+    print(base.attack())
+    print(base.heal())
+    print(" evolved:")
+    print(evolve.describe())
+    print(evolve.attack())
+    print(evolve.heal())
+
+
+def test_transform(factory: TransformCreatureFactory) -> None:
+    base = factory.create_base()
+    evolve = factory.create_evolved()
+    print(" base:")
+    print(base.describe())
+    print(base.attack())
+    print(base.transform())
+    print(base.attack())
+    print(base.revert())
+    print(" evolved:")
+    print(evolve.describe())
+    print(evolve.attack())
+    print(evolve.transform())
+    print(evolve.attack())
+    print(evolve.revert())
+
+
 def main() -> None:
     print("Testing Creature with healing capability")
-    print(" base:")
-    print(" evolved:")
-    print("Testing Creature with transform capability")
-    print(" base:")
-    print(" evolved:")
+    heal_factory = HealingCreatureFactory()
+    test_heal(heal_factory)
+    print("\nTesting Creature with transform capability")
+    transform_factory = TransformCreatureFactory()
+    test_transform(transform_factory)
 
 
 if __name__ == "__main__":
